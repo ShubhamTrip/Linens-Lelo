fab256c4df0d4ae0f8bd52243085ed5812154307
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-22 16:39:12 +0530

    image added

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Nav.css';
+
+export default function Nav(){
+    return(
+        <div className="Navbar">
+          <div className=""> 
+            
+          </div>
+           
+        </div>
+    );
+}

51911466a826b48541d3e09becf906c4cb091cfa
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-22 23:08:17 +0530

    linens lelo updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Nav from './Components/Nav/Nav';
+import Home from './Pages/Home/Home';
+import Products from './Pages/Products/Products';
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+
 function App() {
   return (
     <div className="App">
-        <Nav/>
+        <BrowserRouter>
+          <Nav/>
+          <Routes>
+             <Route path='/'  element={<Home/>}/>
+             <Route path='/Products'  element={<Products/>}/>
+             <Route path='/About'  element={<About/>}/>
+             <Route path='/Contact'  element={<Contact/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import './Nav.css';
+import logo from '../../images/linens.jpeg';
+import {Link} from 'react-router-dom';
 
 export default function Nav(){
+
     return(
-        <div className="Navbar">
-          <div className=""> 
-            <div>
-            <img src="https://github.com/ShubhamTrip/Linens-Lelo-/blob/main/images/linens.png" alt="" />
+        <div className="Navbar"> 
+            <div className="linenicon">
+              <img src={logo} alt="logo"/>
             </div>
-          </div>
-           
+            <div className="navlink">
+                <Link to='/'>Home</Link>
+                <Link to='/Products'>Products</Link>
+                <Link to='/About'>About</Link>
+                <Link to='/Contact'>Contact</Link>
+            </div>
+            <div className="navbutton">
+                <button>Login</button>
+                <button>Register</button>
+                <button>Cart</button>
+            </div>
         </div>
     );
 }

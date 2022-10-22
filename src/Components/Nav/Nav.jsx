c05d4907cd572aeb3e9c7ca016b28814b4b89ef1
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-22 16:45:04 +0530

    linens lelo

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -5,7 +5,9 @@ export default function Nav(){
     return(
         <div className="Navbar">
           <div className=""> 
-            
+            <div>
+            <img src="https://github.com/ShubhamTrip/Linens-Lelo-/blob/main/images/linens.png" alt="" />
+            </div>
           </div>
            
         </div>

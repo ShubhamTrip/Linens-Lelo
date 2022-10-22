d7975fd1685fbff35637f62ed5b16fd29520b285
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-23 00:17:06 +0530

    api added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import './Home.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home(){
 
+    let [products, setproducts]= useState([]);
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json()).then(data=> setproducts(data));
+    },[])
     return(
         <div className='home'>
-             Home
+             <div className='caraosel'>
+                <Carousel
+                showThumbs={false}
+                autoPlay={true}
+                transitionTime={3}
+                infiniteLoop={true}
+                showStatus={false}>
+                {
+                    products.map(data=>(
+                        <div ><img src={data.image}/></div>
+                    ))
+                }  
+                
+
+                </Carousel>
+             </div>
         </div>
     );
 

6b4a65a18aeb9eee643fe088066345882f15dcef
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-11-02 21:05:48 +0530

    product added

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./Product.css";
-
+import { useParams } from "react-router-dom";
 export default function Product(){
+    
+    let  [productinfo,setproductinfo]=useState([]);
+    useEffect({
+      getData
+    },[]);    
+
+    const {id} = useParams();
+
+    useEffect({
+        getData
+    },[id])
+
+     function getData(){
+        fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>setproductinfo(json))
+     }
     return(
         <div className="product">
             <></>
